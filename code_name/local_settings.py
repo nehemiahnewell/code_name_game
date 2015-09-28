964e7dedeb2820a6d92addfea1d136d1e502be23
nehemiahnewell <nehemiahnewell@gmail.com>
2015-09-28 01:25:39 -0700

    Add files

---
+++ code_name/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'Code_NameDB',
+       'USER': 'Nehemiah', #The username you set
+       'PASSWORD': '1111', #The password you set
+       'HOST': '',
+       'PORT': '',
+   }
+}
